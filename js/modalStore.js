0a05d140f23b04dccdc0d3f7dcca17525accd902
DIGORACCOON4279 <dilema4000@gmail.com>
2023-12-08 16:52:11 -0500

    Creating tags bouncing error404

--- js/modalStore.js
+++ js/modalStore.js
@@ -11,8 +11,9 @@ placeOnOrder.addEventListener('submit', (event) => {
         text: "Purchase order number is 526896-589, and we've sent a confirmation to your email.",
         icon: "success",
         iconColor: '#ffffff',
-        iconHtml: '<img src="https://break-a-leg.vercel.app/img/skullLogo.svg" style=" width: 45px; height: 45px">'
+        iconHtml: '<img src="https://break-a-leg.vercel.app/img/skullLogo.svg" style=" width: 45px; height: 45px">',
     });
 });
+
 
 
