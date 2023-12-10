dd32956d823a71e1aa6853698a744202a723b815
DIGORACCOON4279 <dilema4000@gmail.com>
2023-12-10 17:34:54 -0500

    Fixing products-store JSONbin

--- js/main.js
+++ js/main.js
@@ -1,19 +1,44 @@
+// mi codigo
+
+// let outfits = []
+
+// const binID = '6574e4200574da7622d2b981'
+// const apiKey = '$2a$10$yMw6jGD8rgxHutZ46mif6upD3rW7bUaRLJMTw7z/GP7uJQT3d8mP.'
+// const url = `https://api.jsonbin.io/v3/b/${binID}`
+// const headers = {
+//     'secret-key': apiKey
+// }
+
+// fetch(url, { headers })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.record.outfits)
+//     });
 
 
-let outfits = []
 
-const binID = '6574e4200574da7622d2b981'
-const apiKey = '$2a$10$yMw6jGD8rgxHutZ46mif6upD3rW7bUaRLJMTw7z/GP7uJQT3d8mP.'
-const url = `https://api.jsonbin.io/v3/b/${binID}`
-const headers = {
-    'secret-key': apiKey
-}
+// Chat gpt
 
-fetch(url, { headers })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.record.outfits)
-    });
+    let outfits = []
+
+    const binID = '6574e4200574da7622d2b981'
+    const apiKey = '$2a$10$yMw6jGD8rgxHutZ46mif6upD3rW7bUaRLJMTw7z/GP7uJQT3d8mP.'
+    const url = `https://api.jsonbin.io/v3/b/${binID}`
+    const headers = {
+        'secret-key': apiKey
+    }
+
+    fetch(url, { headers })
+        .then(response => response.json())
+        .then(data => {
+            outfits = data.record.outfits;
+            console.log(outfits); // Verificar si los datos se asignaron correctamente
+            renderProducts(outfits); // Llama a la función para renderizar los productos aquí
+        })
+        .catch(error => {
+            console.error('Error al cargar datos desde JSONBin.io:', error);
+        });
+
 
 
 
