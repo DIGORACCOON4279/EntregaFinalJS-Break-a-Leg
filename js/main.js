ad03f88835bac87667fe2ff763abdbdef761136e
DIGORACCOON4279 <dilema4000@gmail.com>
2023-12-11 15:17:41 -0500

    Comment off js/main.js

--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@
 // AJAX JSON fecth jsonbin.io
 
 
-    let outfits = []
+    // let outfits = []
 
-    const binID = '6574e4200574da7622d2b981'
-    const apiKey = '$2a$10$yMw6jGD8rgxHutZ46mif6upD3rW7bUaRLJMTw7z/GP7uJQT3d8mP.'
-    const url = `https://api.jsonbin.io/v3/b/${binID}`
-    const headers = {
-        'secret-key': apiKey
-    }
+    // const binID = '6574e4200574da7622d2b981'
+    // const apiKey = '$2a$10$yMw6jGD8rgxHutZ46mif6upD3rW7bUaRLJMTw7z/GP7uJQT3d8mP.'
+    // const url = `https://api.jsonbin.io/v3/b/${binID}`
+    // const headers = {
+    //     'secret-key': apiKey
+    // }
 
-    fetch(url, { headers })
-        .then(response => response.json())
-        .then(data => {
-            outfits = data.record.outfits;
-            console.log(outfits);
-            renderProducts(outfits);
-        })
-        .catch(error => {
-            console.error('Error al cargar datos desde JSONBin.io:', error);
-        });
+    // fetch(url, { headers })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         outfits = data.record.outfits;
+    //         console.log(outfits);
+    //         renderProducts(outfits);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error al cargar datos desde JSONBin.io:', error);
+    //     });
 
 
 
